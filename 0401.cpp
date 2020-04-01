cac97f83f19bdc3fd32ec7b054b3999574d73d40
Lethe0616 <18792407909@163.com>
2020-04-01 20:01:00 +0800

    0401

---
+++ 0401.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    vector <int> v;
+    v.push_back(1);
+    v.push_back(2);
+    for(int i=2;i<100000;i++)
+    {
+        v.push_back(0);
+        v[i]=(v[i-1]+v[i-2])%1000000;
+    }
+    int n=0;
+    while(cin>>n)
+    {
+        if(n<31)
+            cout<<v[n-1]<<endl;
+        else
+            cout<<setw(6)<<setfill('0')<<v[n-1]<<endl;
+    }
+    return 0;
+}
+
